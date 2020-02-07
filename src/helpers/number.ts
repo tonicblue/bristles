01aaa2fbf185d0fbaebf18d7acd2bd22841520ce
Alex Nuttgens <anuttgens@gmail.com>
2020-02-07 13:59:42 +0000

    tweak to sum so it doesn't return the damn helper!

--- src/helpers/number.ts
+++ src/helpers/number.ts
@@ -8,6 +8,7 @@ export default class NumberHelpers {
   static _sum(...args: any[]): number {
     try {
       const helper: HelperOptions = arguments[arguments.length - 1];
+      args.pop();
       return args.reduce(function(a, b){ return a + b; });
     } catch (err) {
       console.error('Bristles Error -> Helper: sum, Error:', err.message);
